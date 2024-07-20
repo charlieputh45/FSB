1be9f3d91929a344699af86fd9b58d690b41f0bf
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2024-07-20 23:15:55 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -73,6 +73,56 @@ async def extract_movie_info(caption):
     except Exception as e:
         print(e)
     return None, None
+
+async def get_movie_poster(movie_name, release_year, caption):
+    tmdb_search_url = f'https://api.themoviedb.org/3/search/multi?api_key={TMDB_API_KEY}&query={movie_name}'
+    season_no, episode_no = await extract_season_episode(caption)
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(tmdb_search_url) as search_response:
+                search_data = await search_response.json()
+
+                if search_data['results']:
+                    # Filter results based on release year and first air date
+                    matching_results = [
+                        result for result in search_data['results']
+                        if ('release_date' in result and result['release_date'][:4] == str(release_year)) or
+                        ('first_air_date' in result and result['first_air_date'][:4] == str(
+                            release_year))
+                    ]
+
+                    if matching_results:
+                        result = matching_results[0]
+
+                        # Fetch additional details using movie ID
+                        movie_id = result['id']
+                        media_type = result['media_type']
+
+                        tmdb_movie_image_url = f'https://api.themoviedb.org/3/{media_type}/{movie_id}/images?api_key={TMDB_API_KEY}&language=en-US&include_image_language=en,hi'
+
+                        async with session.get(tmdb_movie_image_url) as movie_response:
+                            movie_images = await movie_response.json()
+ 
+
+                        # Use the backdrop_path or poster_path
+                            poster_path = None
+                            if 'backdrops' in movie_images and movie_images['backdrops']:
+                                poster_path = movie_images['backdrops'][0]['file_path']
+                                                        
+                            elif 'backdrop_path' in result and result['backdrop_path']:
+                                poster_path = result['backdrop_path']
+
+                            elif 'poster_path' in result and result['poster_path']:
+                                poster_path = result['poster_path']
+
+                            poster_url = f"https://image.tmdb.org/t/p/original{poster_path}"
+                            return poster_url
+
+    except Exception as e:
+        print(f"Error fetching TMDB data: {e}")
+
+    return None
   
 async def extract_tg_link(telegram_link):
     try:
