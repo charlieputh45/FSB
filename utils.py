bce416981abaaae08a23b956f9fb393319f4ad00
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2024-08-30 18:09:52 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -61,67 +61,6 @@ def humanbytes(size):
     f = ('%.2f' % size).rstrip('0').rstrip('.')
     return f"{f} {suffixes[i]}"
 
-async def extract_movie_info(caption):
-    try:
-        regex = re.compile(r'(.+?)(\d{4})')
-        match = regex.search(caption)
-
-        if match:
-             movie_name = match.group(1).replace('.', ' ').strip()
-             release_year = match.group(2)
-             return movie_name, release_year
-    except Exception as e:
-        print(e)
-    return None, None
-
-async def get_movie_poster(movie_name, release_year):
-    tmdb_search_url = f'https://api.themoviedb.org/3/search/multi?api_key={TMDB_API_KEY}&query={movie_name}'
-
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(tmdb_search_url) as search_response:
-                search_data = await search_response.json()
-
-                if search_data['results']:
-                    # Filter results based on release year and first air date
-                    matching_results = [
-                        result for result in search_data['results']
-                        if ('release_date' in result and result['release_date'][:4] == str(release_year)) or
-                        ('first_air_date' in result and result['first_air_date'][:4] == str(
-                            release_year))
-                    ]
-
-                    if matching_results:
-                        result = matching_results[0]
-
-                        # Fetch additional details using movie ID
-                        movie_id = result['id']
-                        media_type = result['media_type']
-
-                        tmdb_movie_image_url = f'https://api.themoviedb.org/3/{media_type}/{movie_id}/images?api_key={TMDB_API_KEY}&language=en-US&include_image_language=en,hi'
-
-                        async with session.get(tmdb_movie_image_url) as movie_response:
-                            movie_images = await movie_response.json()
- 
-
-                        # Use the backdrop_path or poster_path
-                            poster_path = None
-                            if 'backdrops' in movie_images and movie_images['backdrops']:
-                                poster_path = movie_images['backdrops'][0]['file_path']
-                                                        
-                            elif 'backdrop_path' in result and result['backdrop_path']:
-                                poster_path = result['backdrop_path']
-
-                            elif 'poster_path' in result and result['poster_path']:
-                                poster_path = result['poster_path']
-
-                            poster_url = f"https://image.tmdb.org/t/p/original{poster_path}"
-                            return poster_url
-
-    except Exception as e:
-        print(f"Error fetching TMDB data: {e}")
-
-    return None
   
 async def extract_tg_link(telegram_link):
     try:
@@ -158,7 +97,7 @@ async def download_initial_part(client, media, file_path, chunk_size):
                 break
 '''
 
-async def generate_thumbnail(file_path: str) -> str:
+async def get_duration(file_path: str) -> str:
     try:
         # Use ffprobe to get video duration
         duration_cmd = [
@@ -167,7 +106,6 @@ async def generate_thumbnail(file_path: str) -> str:
         ]
         duration = float(subprocess.check_output(duration_cmd).strip())
         
-        # Generate a random interval
         random_interval = random.uniform(0, duration)
 
         # Adjust the duration to account for a 2% reduction
@@ -187,9 +125,9 @@ async def generate_thumbnail(file_path: str) -> str:
             'ffmpeg', '-ss', str(random_interval), '-i', file_path, 
             '-frames:v', '1', thumbnail_path, '-y'
         ]
-        subprocess.run(thumbnail_cmd, capture_output=True, check=True)
+        #subprocess.run(thumbnail_cmd, capture_output=True, check=True)
 
-        return thumbnail_path, duration
+        return duration
     except Exception as e:
         print(f"Error generating thumbnail: {e}")
         return None        
