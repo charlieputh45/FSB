f48e14760c97cb0927caaff7b674c405ed651b47
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2024-08-30 19:26:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -76,47 +76,62 @@ async def progress(current, total, message, start_time, last_edit_time):
         last_edit_time[0] = time.time()
 
     
-@app.on_message(filters.document | filters.video)
+@app.on_message((filters.document | filters.video))
 async def pyro_task(client, message):
-    custom_thumb = f"downloads/photo.jpg"
     # Send an initial message to display the progress
-    progress_msg = await message.reply_text("Starting download...")
+    dwn_msg = await message.reply_text("Starting download...")
+    file_path = await pyro_download(message, dwn_msg)
 
-    # Record the start time and initialize the last edit time
+    await dwn_msg.delete()
+
+    up_msg = await message.reply_text("Starting upload...")
+    await pyro_upload(message, up_msg, file_path)
+
+async def pyro_download(message, progress_msg):
+
     start_time = time.time()
-    last_edit_time = [start_time]  # Store as list to pass by reference   
+    last_edit_time = [start_time]  # Store as list to pass by reference  
 
     logger.info(f"Downloading {message.caption}...")
+
+    # Download the media and update the progress
+    file_path = await app.download_media(message, 
+                                         progress=progress, 
+                                         progress_args=(progress_msg, start_time, last_edit_time)
+                                         )
+    return file_path
+    
+
+async def pyro_upload(message, progress_msg, file_path):
+    custom_thumb = f"downloads/photo.jpg"
+    start_time = time.time()
+    last_edit_time = [start_time]  # Store as list to pass by reference  
+    duration = await get_duration(file_path)
+
+    logger.info(f"Uploading {message.caption}...")
     try:
         # Download the media and update the progress
-        file_path = await app.download_media(message, 
-                                             progress=progress, 
-                                             progress_args=(progress_msg, start_time, last_edit_time)
-                                             )
-        duration = await get_duration(file_path)
-        logger.info(f"Uploading {message.caption}...")
-
         await app.send_video(chat_id=message.chat.id, 
-                             video=file_path, 
-                             caption=f"<code>{message.caption}</code>", 
-                             duration=duration, 
-                             width=480, 
-                             height=320, 
-                             thumb=custom_thumb, 
-                             progress=progress, 
-                             progress_args=(progress_msg, start_time, last_edit_time))
+                                video=file_path, 
+                                caption=f"<code>{message.caption}</code>", 
+                                duration=duration, 
+                                width=480, 
+                                height=320, 
+                                thumb=custom_thumb, 
+                                progress=progress, 
+                                progress_args=(progress_msg, start_time, last_edit_time))
     except Exception as e:
         logger.error(f"An error occurred: {str(e)}")
         await message.reply_text(f"Failed: {str(e)}")
     finally:
-        # Clean up
-        if os.path.exists(custom_thumb):
-            os.remove(custom_thumb) 
-        if os.path.exists(file_path):
-            os.remove(file_path)
-        # Edit the message to indicate the download is complete
-        await progress_msg.delete() 
-                
+         # Clean up
+         if os.path.exists(custom_thumb):
+             os.remove(custom_thumb)
+         if os.path.exists(file_path):
+             os.remove(file_path)
+         # Edit the message to indicate the download is complete
+         await progress_msg.edit_text('Upload Completed') 
+
 @app.on_message(filters.photo)
 async def get_photo(client, message):
     await app.download_media(message, file_name='photo.jpg')
