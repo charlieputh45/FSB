8a3b00e7066f330fba74f4fd891d4fbace4df57e
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2024-09-03 13:00:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,22 +18,6 @@ logging.getLogger("pyrogram").setLevel(logging.ERROR)
 
 logger = logging.getLogger(__name__)
 
-CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
-try:
-    if len(CONFIG_FILE_URL) == 0:
-        raise TypeError
-    try:
-        res = rget(CONFIG_FILE_URL)
-        if res.status_code == 200:
-            with open('config.env', 'wb+') as f:
-                f.write(res.content)
-        else:
-            logger.error(f"Failed to download config.env {res.status_code}")
-    except Exception as e:
-        logger.info(f"CONFIG_FILE_URL: {e}")
-except:
-    pass
-
 load_dotenv('config.env', override=True)
 
 API_ID = int(os.getenv('API_ID'))
@@ -41,15 +25,10 @@ API_HASH = os.getenv('API_HASH')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 OWNER_USERNAME = os.getenv('OWNER_USERNAME')
 OWNER_ID = int(os.getenv('OWNER_ID'))
-STRING_SESSION = os.getenv('STRING_SESSION')
 
 
-MONGO_URL = os.getenv('MONGO_URL')
-MONGO_DB_NAME = "others"
 
 DB_CHANNEL_ID = int(os.getenv('DB_CHANNEL_ID'))
 LOG_CHANNEL_ID = int(os.getenv('LOG_CHANNEL_ID'))
 CAPTION_CHANNEL_ID = int(os.getenv('CAPTION_CHANNEL_ID'))
 
-URLSHORTX_API_TOKEN = os.getenv('URLSHORTX_API_TOKEN')
-SHORTERNER_URL = os.getenv('SHORTERNER_URL')
