626d766ce1eb775cd9373ee4ce5fb13b9fc53097
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2024-09-03 14:13:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import time
+from pyromod import listen
 from pyrogram import Client, filters, enums
-from pyrogram.errors import FloodWait
 from config import *
 from utils import *
+
 
 app = Client(
     "my_bot",
@@ -45,29 +46,35 @@ async def pyro_task(client, message):
     last_edit_time = [start_time]  # Store as list to pass by reference
     last_data = [0]  # Track the last amount of data transferred
     caption = message.caption
-    thumb_path = None 
-    file_path = None
     
-    if message.photo:
-        thumb_path = await app.download_media(message, file_name=f'photo{message.id}.jpg')
-        await message.delete()
-        return
+    await asyncio.sleep(3)
+    rply = await message.reply_text("Please send a photo")
+    # Listen for a photo message
+    photo_msg = await app.listen(message.chat.id, filters=filters.photo)
+    
+    await asyncio.sleep(3)
+    thumb_path = await app.download_media(photo_msg, file_name=f'photo_{message.id}.jpg')
+    await photo_msg.delete()
+    
+    # Send an initial message to display the progress
+    await asyncio.sleep(3)
+    progress_msg = await rply.edit_text("Starting download...")
     
     try:
-        # Check if the custom thumbnail exists
-        if thumb_path is None or not os.path.exists(thumb_path):
-            await message.reply_text("Please set a custom thumbnail first.")
-            return
-            
-        # Send an initial message to display the progress
-        progress_msg = await message.reply_text("Starting download...") 
-        
+        await asyncio.sleep(3)
         # Download the media and update the progress
         file_path = await app.download_media(message, file_name=f"{caption}", 
                                              progress=progress, progress_args=(progress_msg, last_edit_time, last_data))
         
         duration = await get_duration(file_path)
-                    
+        
+        # Check if the custom thumbnail exists
+        if not os.path.exists(thumb_path):
+            await asyncio.sleep(3)
+            await message.reply_text("Please set a custom thumbnail first.")
+            return
+            
+        await asyncio.sleep(3)  
         send_msg = await app.send_video(DB_CHANNEL_ID, 
                                         video=file_path, 
                                         caption=f"<code>{message.caption}</code>", 
@@ -77,23 +84,20 @@ async def pyro_task(client, message):
                                         thumb=thumb_path, 
                                         progress=progress, 
                                         progress_args=(progress_msg, last_edit_time, last_data))
-        
-        await progress_msg.edit_text("Uploaded ✅")
         await asyncio.sleep(3)
+        await progress_msg.edit_text("Uploaded ✅")
 
         new_caption = await remove_unwanted(caption)
         file_info = f"🎞️ <b>{new_caption}</b>\n\n🆔 <code>{send_msg.id}</code>"
-        await app.send_photo(CAPTION_CHANNEL_ID, thumb_path, caption=file_info)
         await asyncio.sleep(3)
+        await app.send_photo(CAPTION_CHANNEL_ID, thumb_path, caption=file_info)
         
     except Exception as e:
         logger.error(f'{e}')
-    except FloodWait as f:
-        await asyncio.sleep(f.value)
     finally:
-        if file_path and os.path.exists(file_path):
+        if not os.path.exists(file_path):
             os.remove(file_path)
-        if thumb_path and os.path.exists(thumb_path):
+        if not os.path.exists(thumb_path):
             os.remove(thumb_path)
 
 app.run()
