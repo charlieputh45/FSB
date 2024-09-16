6ae9ba5dab49d5519ad04741bb02dd5a54e84b85
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2024-09-17 02:32:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ async def send_msg(client, message):
 
                         await asyncio.sleep(3)
 
-        await message.reply_text("Messages send successfully!")
+        await message.reply_text("Messages send successfully ✅")
 
     except FloodWait as e:
         await asyncio.sleep(e.value)
