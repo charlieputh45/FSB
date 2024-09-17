8807b10ee166b7c06d8b8e9f946476cf2728951e
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2024-09-17 12:18:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import uuid
 from utils import *
 from config import *
-from shorterner import *
+from html import escape
 from time import time as tm
 from pyrogram import idle
 from pyromod import listen
 from pyrogram.errors import FloodWait
 from pyrogram import Client, filters, enums
+from shorterner import *
 from asyncio import get_event_loop
 from pymongo import MongoClient
 from pyrogram.types import User
@@ -20,7 +21,7 @@ mongo_db = mongo_client[MONGO_DB_NAME]
 mongo_collection = mongo_db[MONGO_COLLECTION]
 
 user_data = {}
-TOKEN_TIMEOUT = 7200
+TOKEN_TIMEOUT = 28800
 
 app = Client(
     "my_bot",
@@ -28,8 +29,8 @@ app = Client(
       api_hash=API_HASH, 
       bot_token=BOT_TOKEN, 
       workers=1000, 
-      parse_mode=enums.ParseMode.HTML,
-      in_memory=True)
+      parse_mode=enums.ParseMode.HTML
+      )
 
 user = Client(
                 "userbot",
@@ -46,15 +47,14 @@ async def main():
 with app:
     bot_username = (app.get_me()).username
 
-        
 @app.on_message(filters.private & filters.command("start"))
-async def get_command(client, message):
-    user_id = message.from_user.id
-    user_link = await get_user_link(message.from_user)
+async def get_command(client, message): 
+     user_id = message.from_user.id
+     user_link = await get_user_link(message.from_user)
 
-    if len(message.command) > 1 and message.command[1] == "token":
+     if len(message.command) > 1 and message.command[1] == "token":
         try:
-            file_id = 959
+            file_id = 4
             get_msg = await app.get_messages(DB_CHANNEL_ID, int(file_id))
             cpy_msg = await get_msg.copy(chat_id=message.chat.id)
             await auto_delete_message(message, cpy_msg)
@@ -63,53 +63,15 @@ async def get_command(client, message):
             logger.error(f"{e}")
         return
 
-    if len(message.command) > 1 and len(message.command[1]) == 36:
-        input_token = message.command[1] if len(message.command) > 1 else None
-        token_msg = await verify_token(user_id, input_token)
-        reply = await message.reply_text(token_msg)
-        await app.send_message(LOG_CHANNEL_ID, f"User🕵️‍♂️{user_link} with 🆔 {user_id} @{bot_username} {token_msg}", parse_mode=enums.ParseMode.HTML)
-        await auto_delete_message(message, reply)
-        return
-
-    file_id = message.command[1] if len(message.command) > 1 else None
-
-    if file_id:
-        if not await check_access(message, user_id):
-            return
-        try:
-            file_message = await app.get_messages(DB_CHANNEL_ID, int(file_id))
-            media = file_message.video or file_message.audio or file_message.document
-            if media:
-                caption = file_message.caption if file_message.caption else None
-                if caption:
-                    new_caption = await remove_extension(caption.html)
-                    copy_message = await file_message.copy(chat_id=message.chat.id, caption=f"<code>{new_caption}</code>", parse_mode=enums.ParseMode.HTML)
-                    user_data[user_id]['file_count'] = user_data[user_id].get('file_count', 0) + 1
-                else:
-                    copy_message = await file_message.copy(chat_id=message.chat.id)
-                    user_data[user_id]['file_count'] = user_data[user_id].get('file_count', 0) + 1
-                await auto_delete_message(message, copy_message)
-                await asyncio.sleep(3)
-            else:
-                reply = await message.reply_text("File not found or inaccessible.")
-                await auto_delete_message(message, reply)
-
-        except ValueError:
-            reply = await message.reply_text("Invalid File ID") 
-            await auto_delete_message(message, reply)  
-
-        except FloodWait as f:
-            await asyncio.sleep(f.value)
-            if caption:
-                copy_message = await file_message.copy(chat_id=message.chat.id, caption=f"<code>{new_caption}</code>", parse_mode=enums.ParseMode.HTML)
-                user_data[user_id]['file_count'] = user_data[user_id].get('file_count', 0) + 1
-            else:
-                copy_message = await file_message.copy(chat_id=message.chat.id)
-                user_data[user_id]['file_count'] = user_data[user_id].get('file_count', 0) + 1
-
-            await auto_delete_message(message, copy_message)
-            await asyncio.sleep(3)
-    else:
+     if len(message.command) > 1 and len(message.command[1]) == 36:
+         input_token = message.command[1] if len(message.command) > 1 else None
+         token_msg = await verify_token(user_id, input_token)
+         reply = await message.reply_text(token_msg)
+         await app.send_message(LOG_CHANNEL_ID, f"User🕵️‍♂️{user_link} with 🆔 {user_id} @{bot_username} {token_msg}", parse_mode=enums.ParseMode.HTML)
+         await auto_delete_message(message, reply)
+         return
+     
+     else:
         mongo_collection.update_one(
                 {'user_id': user_id},
                 {'$set': {'user_id': user_id}}, 
@@ -118,15 +80,74 @@ async def get_command(client, message):
         reply = await message.reply_text(f"<b>💐Welcome this is TG⚡️Flix Bot")
         await auto_delete_message(message, reply)
 
+@app.on_message(filters.private & filters.command("get"))
+async def handle_get_command(client, message):
+    user_id = message.from_user.id
+    if not await check_access(message, user_id):
+         return
+    
+    file_id = message.command[1] if len(message.command) > 1 else None
+
+    if file_id:
+        try:
+            file_message = await app.get_messages(DB_CHANNEL_ID, int(file_id))
+            media = file_message.video or file_message.audio or file_message.document
+            if media:
+                caption = file_message.caption if file_message.caption else None
+                if caption:
+                    new_caption = await remove_extension(caption.html)
+                    copy_message = await file_message.copy(chat_id=message.chat.id, caption=f"<b>{escape(new_caption)}</b>", parse_mode=enums.ParseMode.HTML)
+                    user_data[user_id]['file_count'] = user_data[user_id].get('file_count', 0) + 1
+
+                else:
+                    copy_message = await file_message.copy(chat_id=message.chat.id)
+                    user_data[user_id]['file_count'] = user_data[user_id].get('file_count', 0) + 1
+
+                await auto_delete_message(message, copy_message)
+
+                await asyncio.sleep(3)
+
+            else:
+                 reply = await message.reply_text("File not found or inaccessible.")
+                 await auto_delete_message(message, reply)
+
+        except ValueError:
+                reply = await message.reply_text("Invalid File ID") 
+                await auto_delete_message(message, reply)  
+
+        except FloodWait as f:
+            await asyncio.sleep(f.value)
+            if caption:
+                copy_message = await file_message.copy(chat_id=message.chat.id, caption=f"<b>{escape(new_caption)}</b>", parse_mode=enums.ParseMode.HTML)
+                user_data[user_id]['file_count'] = user_data[user_id].get('file_count', 0) + 1
+
+            else:
+                copy_message = await file_message.copy(chat_id=message.chat.id)
+                user_data[user_id]['file_count'] = user_data[user_id].get('file_count', 0) + 1
+
+
+            await auto_delete_message(message, copy_message)
+            user_data[user_id]['file_count'] += 1
+            await asyncio.sleep(3)
+    else:
+        reply = await message.reply_text("Provide a File Id")
+        await auto_delete_message(message, reply)  
+
+@app.on_message(filters.private & (filters.document | filters.video) & filters.user(OWNER_USERNAME))
+async def forward_message_to_new_channel(client, message):
+    if message.photo:
+        message.copy(CAPTION_CHANNEL_ID)
+
 
 @app.on_message(filters.command("copy") & filters.user(OWNER_USERNAME))
 async def copy_msg(client, message):    
     try:
+        await message.delete()
         async def get_user_input(prompt):
             rply = await message.reply_text(prompt)
-            await rply.delete()
             link_msg = await app.listen(message.chat.id)
             await link_msg.delete()
+            await rply.delete()
             return link_msg.text
         
         # Collect input from the user
@@ -135,16 +156,19 @@ async def copy_msg(client, message):
         db_channel_id = int(await extract_channel_id(await get_user_input("Send db_channel link")))
         destination_id = int(await extract_channel_id(await get_user_input("Send destination channel link")))
 
-        # Get and copy messages
-        file_messages = await app.get_messages(db_channel_id, range(start_msg_id, end_msg_id + 1))
+        batch_size = 199
+        for start in range(start_msg_id, end_msg_id + 1, batch_size):
+            end = min(start + batch_size - 1, end_msg_id)  # Ensure we don't go beyond end_msg_id
+            # Get and copy messages
+            file_messages = await app.get_messages(db_channel_id, range(start, end + 1))
 
-        for file_message in file_messages:
-            if file_message and (file_message.document or file_message.video or file_message.audio or file_message.photo):
-                caption = file_message.caption.html if file_message.caption else None
-                await file_message.copy(destination_id, caption=caption)
-                await asyncio.sleep(3)
-                
-        await message.reply_text("Messages copied successfully!")
+            for file_message in file_messages:
+                if file_message and (file_message.document or file_message.video or file_message.audio or file_message.photo):
+                    caption = file_message.caption.html if file_message.caption else None
+                    await file_message.copy(destination_id, caption=caption)
+                    await asyncio.sleep(3)
+                    
+        await message.reply_text("Messages copied successfully!✅")
         
     except FloodWait as e:
         await asyncio.sleep(e.value)
@@ -153,7 +177,7 @@ async def copy_msg(client, message):
 
 # Delete Commmand
 @app.on_message(filters.command("delete") & filters.user(OWNER_USERNAME))
-async def get_command(client, message):
+async def delete_command(client, message):
     try:
         await message.reply_text("Enter channel_id")
         channel_id = int((await app.listen(message.chat.id)).text)
@@ -183,7 +207,17 @@ async def log_command(client, message):
         await auto_delete_message(message, reply)
     except Exception as e:
         await app.send_message(user_id, f"Failed to send log file. Error: {str(e)}")
-    
+
+# Get Total User Command
+@app.on_message(filters.command("users") & filters.user(OWNER_USERNAME))
+async def total_users_command(client, message):
+    user_id = message.from_user.id
+
+    total_users = mongo_collection.count_documents({})
+    response_text = f"Total number of users in the database: {total_users}"
+    reply = await app.send_message(user_id, response_text)
+    await auto_delete_message(message, reply)
+            
 async def verify_token(user_id, input_token):
     current_time = tm()
 
