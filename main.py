5ab3b7242e6a36f9556d56a4b1e07d353801eed2
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2024-09-18 16:06:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ async def get_command(client, message):
 
     if len(message.command) > 1 and message.command[1] == "token":
         try:
-            file_id = 1366
+            file_id = 963
             get_msg = await app.get_messages(DB_CHANNEL_ID, int(file_id))
             cpy_msg = await get_msg.copy(chat_id=message.chat.id)
             await message.delete()
