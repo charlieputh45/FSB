3d0863dd69f691ae89120524ce989ad59845e55a
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2024-09-30 18:12:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ async def handle_media_message(client, message):
         for start in range(start_msg_id, end_msg_id + 1, batch_size):
             end = min(start + batch_size - 1, end_msg_id)
             # Get and copy messages
-            file_messages = await app.get_messages(db_channel_id, range(start, end + 1))
+            file_messages = await app.get_messages(DB_CHANNEL_ID, range(start, end + 1))
 
             for file_message in file_messages:
                 media = file_message.document or file_message.video
