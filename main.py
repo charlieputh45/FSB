d66be524322abe94c188c660b7891f1818d5e07f
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2024-10-05 23:45:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,146 +1,172 @@
-import asyncio
-import imgbbpy
-import requests
-from inspect import signature
-from datetime import datetime, timezone
-from config import *
-from pymongo import MongoClient
+import os 
 from utils import *
+from config import *
+from html import escape
 from pyrogram import idle
 from pyromod import listen
-from pyrogram import Client, filters, enums, utils as pyroutils
 from pyrogram.errors import FloodWait
-from status import *
+from pyrogram import Client, filters, enums
 from asyncio import get_event_loop
 
+DOWNLOAD_PATH = "downloads/"
+loop = get_event_loop()
 
-# Initialize the client with your API key
-imgclient = imgbbpy.SyncClient(IMGBB_API_KEY)
+os.makedirs(DOWNLOAD_PATH, exist_ok=True)
 
-# Initialize MongoDB client
-MONGO_COLLECTION = "users"
-mongo_client = MongoClient(MONGO_URI)  
-db = mongo_client[MONGO_DB_NAME]
-collection = db[COLLECTION_NAME]
-mongo_collection = db[MONGO_COLLECTION]
-
-pyroutils.MIN_CHAT_ID = -999999999999
-pyroutils.MIN_CHANNEL_ID = -100999999999999
-
-
-THUMBNAIL_COUNT = 9
-GRID_COLUMNS = 3 # Number of columns in the grid
+user_data = {}
+TOKEN_TIMEOUT = 7200
 
 app = Client(
     "my_bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    workers=1000,
-    parse_mode=enums.ParseMode.HTML
-)
- 
+      api_id=API_ID,
+      api_hash=API_HASH, 
+      bot_token=BOT_TOKEN, 
+      workers=1000, 
+      parse_mode=enums.ParseMode.HTML,
+      in_memory=True)
+
+user = Client(
+                "userbot",
+                api_id=int(API_ID),
+                api_hash=API_HASH,
+                session_string=STRING_SESSION,
+                no_updates = True
+            )
+
+async def main():
+    async with app, user:
+        await idle()
+
 with app:
     bot_username = (app.get_me()).username
 
-def wztgClient(*args, **kwargs):
-    if 'max_concurrent_transmissions' in signature(Client.__init__).parameters:
-        kwargs['max_concurrent_transmissions'] = 1000
-    return Client(*args, **kwargs)
-
-@app.on_message(filters.command("send") & filters.user(OWNER_USERNAME))
-async def handle_media_message(client, message):
-    user_id = message.from_user.id
+@app.on_message(filters.private & (filters.document | filters.video))
+async def forward_message_to_new_channel(client, message):
     try:
-        async def get_user_input(prompt):
-            rply = await message.reply_text(prompt)
-            link_msg = await app.listen(message.chat.id)
-            return link_msg.text
+        media = message.document or message.video
+        file_id = message.id
+        file_size = media.file_size
 
-        # Collect input from the user
-        start_msg_id = int(await extract_tg_link(await get_user_input("Send first post link")))
-        end_msg_id = int(await extract_tg_link(await get_user_input("Send end post link")))
+        if media:
+            caption = message.caption if message.caption else None
 
-        batch_size = 199
-        for start in range(start_msg_id, end_msg_id + 1, batch_size):
-            end = min(start + batch_size - 1, end_msg_id)
-            # Get and copy messages
-            file_messages = await app.get_messages(DB_CHANNEL_ID, range(start, end + 1))
+            if caption:
+                new_caption = await remove_unwanted(caption)
 
-            for file_message in file_messages:
-                media = file_message.document or file_message.video
+                # Generate file path
+                logger.info(f"Downloading initial part of {file_id}...")
                 
-                if media:
-                    caption = file_message.caption if file_message.caption else None
+                dwnld_msg = await message.reply_text("📥 Downloading")
+                
+                file_path = await app.download_media(message, file_name=f"{caption}")
+                print("Generating Thumbnail")
+                # Generate a thumbnail
+                movie_name, release_year = await extract_movie_info(caption)
+                thumbnail_path = await get_movie_poster(movie_name, release_year)
+                duration = await generate_duration(file_path)
 
-                    if caption:
-                        file_name = await remove_unwanted(caption)
+                if thumbnail_path:
+                    print(f"Thumbnail generated: {thumbnail_path}")
+                else:
+                    print("Failed to generate thumbnail")   
 
-                        logger.info(f"Starting download of {file_message.id}...")
 
-                        # Download media with progress updates
-                        file_path = await app.download_media(
-                            file_message, 
-                            file_name=f"{file_message.id}", 
-                            progress=progress 
-                        )
+                upld_msg = await dwnld_msg.edit_text("⏫ Uploading")
+                send_msg = await app.send_video(DB_CHANNEL_ID, 
+                                                video=file_path, 
+                                                caption=f"<code>{escape(caption)}</code>",
+                                                duration=duration, 
+                                                width=480, 
+                                                height=320, 
+                                                thumb=thumbnail_path
+                                               )
+                
+                await upld_msg.edit_text("Uploaded ✅")
 
-                        # Generate thumbnails after downloading
-                        screenshots, thumbnail, duration = await generate_combined_thumbnail(file_path, THUMBNAIL_COUNT, GRID_COLUMNS)
+                file_info = f"<b>🗂️ {escape(new_caption)}\n\n💾 {humanbytes(file_size)}   🆔 <code>{send_msg.id}</code></b>"
 
-                        if screenshots and thumbnail:
-                            logger.info(f"Thumbnail generated: {screenshots}")
+                await app.send_photo(CAPTION_CHANNEL_ID, thumbnail_path, caption=file_info)
 
-                            file_info = {
-                                "file_id": file_message.id, 
-                                "file_name": file_name, 
-                                "file_size": humanbytes(media.file_size)
-                            }
+                os.remove(thumbnail_path)
+                os.remove(file_path)
 
-                            try:
-                                # Upload thumbnail and screenshots to ImgBB
-                                thumb = imgclient.upload(file=f"{thumbnail}")
-                                os.remove(thumbnail)
-
-                                await asyncio.sleep(5)
-
-                                ss = imgclient.upload(file=f"{screenshots}")
-                                os.remove(screenshots)
-                                                        
-                                # Create the document to store in MongoDB
-                                document = {
-                                    "file_info": file_info,
-                                    "thumbnail_url": thumb.url, 
-                                    "screenshot_url": ss.url, 
-                                    "timestamp": datetime.now(timezone.utc).isoformat()
-                                }
-
-                                if thumb:
-                                    # Insert into MongoDB
-                                    try:
-                                        collection.insert_one(document)
-                                        logger.info(f"File {file_name} uploaded and data saved successfully.")
-                                    except Exception as e:
-                                        logger.error(f"Error in handle_media_message: {e}")
-                                        await app.send_message(user_id, text=f"An error occurred while adding the file information {file_name}")
-
-                            except Exception as e:
-                                await app.send_message(user_id, text=f"Failed to upload the video thumbnail for {file_name}. Please try again.")
-                                logger.error(f"Error uploading video thumbnail: {e}")
-
-                    await asyncio.sleep(3)  # To prevent rate limiting
+                await asyncio.sleep(3)
 
     except Exception as e:
-        logger.error(f"Error in handle_media_message: {e}")
-        await message.reply_text("An unexpected error occurred.") 
+        logger.error(f'{e}') 
     finally:
         if os.path.exists(file_path):
             os.remove(file_path)
+        if os.path.exists(thumbnail_path):
+            os.remove(thumbnail_path)
+        
+@app.on_message(filters.command("set"))
+async def forward_message_to_new_channel(client, message):
+    try:
+        media = message.document or message.video
+        file_id = message.id
+        file_size = media.file_size
+
+        if media:
+            caption = message.caption if message.caption else None
+
+            if caption:
+                new_caption = await remove_unwanted(caption)
+
+                # Generate file path
+                logger.info(f"Downloading initial part of {file_id}...")
+                
+                dwnld_msg = await message.reply_text("📥 Downloading")
+                
+                file_path = await app.download_media(message, file_name=f"{caption}")
+                print("Generating Thumbnail")
+                # Generate a thumbnail
+                rply = await message.reply_text(f"Please send a photo")
+                photo_msg = await app.listen(message.chat.id, filters=filters.photo)
+                thumbnail_path = await app.download_media(photo_msg, file_name=f'photo_{message.id}.jpg')
+                await rply.delete()
+                await photo_msg.delete()
+                
+                duration = await generate_duration(file_path)
+
+                if thumbnail_path:
+                    print(f"Thumbnail generated: {thumbnail_path}")
+                else:
+                    print("Failed to generate thumbnail")   
 
 
-@app.on_message(filters.private & filters.command("start"))
-async def start_command(client, message):
-    reply = await message.reply_text(f"<b>💐Welcome to FileShare Bot")
+                upld_msg = await dwnld_msg.edit_text("⏫ Uploading")
+                send_msg = await app.send_video(DB_CHANNEL_ID, 
+                                                video=file_path, 
+                                                caption=f"<code>{escape(caption)}</code>",
+                                                duration=duration, 
+                                                width=480, 
+                                                height=320, 
+                                                thumb=thumbnail_path,
+                                               )
+                
+                await upld_msg.edit_text("Uploaded ✅")
+
+                file_info = f"<b>🗂️ {escape(new_caption)}\n\n💾 {humanbytes(file_size)}   🆔 <code>{send_msg.id}</code></b>"
+
+                await app.send_photo(CAPTION_CHANNEL_ID, thumbnail_path, caption=file_info)
+
+                os.remove(thumbnail_path)
+                os.remove(file_path)
+
+                await asyncio.sleep(3)
+
+    except Exception as e:
+        logger.error(f'{e}') 
+    finally:
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        if os.path.exists(thumbnail_path):
+            os.remove(thumbnail_path)
+
+@app.on_message(filters.command("start"))
+async def get_command(client, message):
+    reply = await message.reply_text(f"<b>💐Welcome this is TG⚡️Flix Bot")
     await auto_delete_message(message, reply)
 
 
@@ -155,42 +181,7 @@ async def log_command(client, message):
         await auto_delete_message(message, reply)
     except Exception as e:
         await app.send_message(user_id, f"Failed to send log file. Error: {str(e)}")
-                                    
-@app.on_message(filters.command("copy") & filters.user(OWNER_USERNAME))
-async def copy_msg(client, message):    
-    try:
-        await message.delete()
-        async def get_user_input(prompt):
-            rply = await message.reply_text(prompt)
-            link_msg = await app.listen(message.chat.id)
-            await rply.delete()
-            return link_msg.text
-        
-        # Collect input from the user
-        start_msg_id = int(await extract_tg_link(await get_user_input("Send first post link")))
-        end_msg_id = int(await extract_tg_link(await get_user_input("Send end post link")))
-        db_channel_id = int(await extract_channel_id(await get_user_input("Send db_channel link")))
-        destination_id = int(await extract_channel_id(await get_user_input("Send destination channel link")))
-
-        batch_size = 199
-        for start in range(start_msg_id, end_msg_id + 1, batch_size):
-            end = min(start + batch_size - 1, end_msg_id)  # Ensure we don't go beyond end_msg_id
-            # Get and copy messages
-            file_messages = await app.get_messages(db_channel_id, range(start, end + 1))
-
-            for file_message in file_messages:
-                if file_message and (file_message.document or file_message.video or file_message.audio or file_message.photo):
-                    caption = file_message.caption
-                    await file_message.copy(destination_id, caption=f"<b>{caption}</b>")
-                    await asyncio.sleep(3)
-                    
-        await message.reply_text("Messages copied successfully!✅")
-        
-    except FloodWait as e:
-        await asyncio.sleep(e.value)
-    except Exception as e:
-        logger.error(f'{e}')
-
-logger.info("Bot is starting...")
-app.run()
-        
+    
+      
+if __name__ == "__main__":
+    loop.run_until_complete(main())
