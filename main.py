618f8ffc9289f56b7dd2d7fd28de9e6bed35bda4
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2024-10-05 23:45:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import imgbbpy
 import requests
 from inspect import signature
 from datetime import datetime, timezone
@@ -12,6 +13,10 @@ from pyrogram.errors import FloodWait
 from status import *
 from asyncio import get_event_loop
 
+
+# Initialize the client with your API key
+imgclient = imgbbpy.SyncClient(IMGBB_API_KEY)
+
 # Initialize MongoDB client
 MONGO_COLLECTION = "users"
 mongo_client = MongoClient(MONGO_URI)  
@@ -22,7 +27,7 @@ mongo_collection = db[MONGO_COLLECTION]
 pyroutils.MIN_CHAT_ID = -999999999999
 pyroutils.MIN_CHANNEL_ID = -100999999999999
 
-loop = get_event_loop()
+
 THUMBNAIL_COUNT = 9
 GRID_COLUMNS = 3 # Number of columns in the grid
 
@@ -34,7 +39,7 @@ app = Client(
     workers=1000,
     parse_mode=enums.ParseMode.HTML
 )
-        
+ 
 with app:
     bot_username = (app.get_me()).username
 
@@ -83,7 +88,7 @@ async def handle_media_message(client, message):
                         # Generate thumbnails after downloading
                         screenshots, thumbnail, duration = await generate_combined_thumbnail(file_path, THUMBNAIL_COUNT, GRID_COLUMNS)
 
-                        if screenshots:
+                        if screenshots and thumbnail:
                             logger.info(f"Thumbnail generated: {screenshots}")
 
                             file_info = {
@@ -94,30 +99,33 @@ async def handle_media_message(client, message):
 
                             try:
                                 # Upload thumbnail and screenshots to ImgBB
-                                thumb_url = await upload_to_imgbb(thumbnail)
+                                thumb = imgclient.upload(file=f"{thumbnail}")
                                 os.remove(thumbnail)
-                        
-                                screenshot_url = await upload_to_imgbb(screenshots)
+
+                                await asyncio.sleep(5)
+
+                                ss = imgclient.upload(file=f"{screenshots}")
                                 os.remove(screenshots)
-                        
+                                                        
                                 # Create the document to store in MongoDB
                                 document = {
                                     "file_info": file_info,
-                                    "thumbnail_url": thumb_url, 
-                                    "screenshot_url": screenshot_url, 
+                                    "thumbnail_url": thumb.url, 
+                                    "screenshot_url": ss.url, 
                                     "timestamp": datetime.now(timezone.utc).isoformat()
                                 }
 
-                                # Insert into MongoDB
-                                try:
-                                    result = collection.insert_one(document)
-                                    logger.info(f"File {file_name} uploaded and data saved successfully.")
-                                except Exception as e:
-                                    logger.error(f"Error in handle_media_message: {e}")
-                                    await app.send_message(user_id, text=f"An error occurred while adding the file information {file_name}")
+                                if thumb:
+                                    # Insert into MongoDB
+                                    try:
+                                        collection.insert_one(document)
+                                        logger.info(f"File {file_name} uploaded and data saved successfully.")
+                                    except Exception as e:
+                                        logger.error(f"Error in handle_media_message: {e}")
+                                        await app.send_message(user_id, text=f"An error occurred while adding the file information {file_name}")
 
                             except Exception as e:
-                                await app.send_message(user_id, text_id=f"Failed to upload the video thumbnail for {file_name}. Please try again.")
+                                await app.send_message(user_id, text=f"Failed to upload the video thumbnail for {file_name}. Please try again.")
                                 logger.error(f"Error uploading video thumbnail: {e}")
 
                     await asyncio.sleep(3)  # To prevent rate limiting
@@ -147,36 +155,7 @@ async def log_command(client, message):
         await auto_delete_message(message, reply)
     except Exception as e:
         await app.send_message(user_id, f"Failed to send log file. Error: {str(e)}")
-
-async def upload_to_imgbb(image_path, expiration=None):
-    url = "https://api.imgbb.com/1/upload"
-    with open(image_path, "rb") as file:
-        files = {
-            "image": file,
-        }
-        payload = {
-            "key": IMGBB_API_KEY,
-        }
-
-        # Add expiration if provided
-        if expiration:
-            payload["expiration"] = expiration
-        
-        response = requests.post(url, data=payload, files=files)
-        
-        # Check for successful response
-        if response.status_code == 200:
-            result = response.json()
-            if result.get('success'):
-                return result['data']['url']  # Return the direct image URL
-            else:
-                error_message = result.get('error', 'Unknown error')
-                logger.error(f"ImgBB upload failed: {error_message}")
-                return None
-        else:
-            logger.error(f"ImgBB API error: {response.status_code}")
-            return None
-
+                                    
 @app.on_message(filters.command("copy") & filters.user(OWNER_USERNAME))
 async def copy_msg(client, message):    
     try:
@@ -211,6 +190,7 @@ async def copy_msg(client, message):
         await asyncio.sleep(e.value)
     except Exception as e:
         logger.error(f'{e}')
-        
+
 logger.info("Bot is starting...")
 app.run()
+        
